21d0e8c10d5255043e7b9a2a22af908f5b15f589
Tanisha Bera <tanisha_bera@epam.com>
2026-01-30 16:30:29 +0530

    Theory related to exception propagation added

--- src/main/java/org/child1/TrickyQuestions/Exceptions/Example2/Main.java
+++ src/main/java/org/child1/TrickyQuestions/Exceptions/Example2/Main.java
@@ -17,3 +17,10 @@ public class Main {
 //Answer:
 //2
 //“Finally overrides return from try or catch.”
+
+//Step-by-step:
+//try block runs, hits return 1; (Java prepares to return 1).
+//Before returning, Java enters the finally block.
+//finally block runs, hits return 2; (Java now prepares to return 2, overriding the previous
+// return).
+//The method returns 2.
